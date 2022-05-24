c20e7119ac0d522c23f9773679eed52dc27092ab
zhaowei <choui20016514@gmail.com>
2022-05-24 19:38:52 +0800

    🐛  cors supports

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var express = require('express');
-const { generateToken04 } = require('./server/zegoServerAssistant');
+var express = require("express");
+var cors = require("cors");
+const { generateToken04 } = require("./server/zegoServerAssistant");
 
 var PORT = process.env.PORT || 8080;
 
 if (!(process.env.APP_ID && process.env.SERVER_SECRET)) {
-    throw new Error('You must define an APP_ID and SERVER_SECRET');
+  throw new Error("You must define an APP_ID and SERVER_SECRET");
 }
 var APP_ID = process.env.APP_ID;
 var SERVER_SECRET = process.env.SERVER_SECRET;
@@ -12,33 +13,41 @@ var SERVER_SECRET = process.env.SERVER_SECRET;
 var app = express();
 
 function nocache(req, res, next) {
-    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-    res.header('Expires', '-1');
-    res.header('Pragma', 'no-cache');
-    next();
+  res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
+  res.header("Expires", "-1");
+  res.header("Pragma", "no-cache");
+  next();
 }
 
 var generateAccessToken = function (req, resp) {
-    resp.header('Access-Control-Allow-Origin', "*")
+  resp.header("Access-Control-Allow-Origin", "*");
 
-    var expiredTs = req.query.expired_ts;
-    if (!expiredTs) {
-        expiredTs = 3600;
-    }
+  var expiredTs = req.query.expired_ts;
+  if (!expiredTs) {
+    expiredTs = 3600;
+  }
 
-    var uid = req.query.uid;
-    if (!uid) {
-        return resp.status(500).json({ 'error': 'user id is required' });
-    }
+  var uid = req.query.uid;
+  if (!uid) {
+    return resp.status(500).json({ error: "user id is required" });
+  }
 
-    const token =  generateToken04(parseInt(APP_ID), uid, SERVER_SECRET, parseInt(expiredTs), '');
-    return resp.json({ 'token': token });
+  const token = generateToken04(
+    parseInt(APP_ID),
+    uid,
+    SERVER_SECRET,
+    parseInt(expiredTs),
+    ""
+  );
+  return resp.json({ token: token });
 };
-
-app.get('/access_token', nocache, generateAccessToken);
+app.use(cors());
+app.get("/access_token", nocache, generateAccessToken);
 
 app.listen(PORT, function () {
-    console.log('Service URL http://127.0.0.1:' + PORT + "/");
-    console.log('Token request, /access_token?uid=[user id]');
-    console.log('Token with expiring time request, /access_token?uid=[user id]&expiredTs=[expire ts]');
+  console.log("Service URL http://127.0.0.1:" + PORT + "/");
+  console.log("Token request, /access_token?uid=[user id]");
+  console.log(
+    "Token with expiring time request, /access_token?uid=[user id]&expiredTs=[expire ts]"
+  );
 });
